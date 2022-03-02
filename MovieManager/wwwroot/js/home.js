3647202b98d3882a412bf92bb2b0a21d6924c06c
ivaaak <201218024@fdiba.tu-sofia.bg>
2022-03-02 23:25:19 +0200

    modified home page - scroll arrow + animation

--- MovieManager/wwwroot/js/home.js
+++ MovieManager/wwwroot/js/home.js
@@ -1,3 +1,32 @@
 $('.menu-button').click(function () {
 	$('.menu').toggleClass('menu-active');
 });
+
+// opacity of the arrow
+$(window).scroll(function () {
+    var topWindow = $(window).scrollTop();
+    var topWindow = topWindow * 1.5;
+    var windowHeight = $(window).height();
+    var position = topWindow / windowHeight;
+    position = 1 - position;
+    //define arrow opacity based on the scroll %
+    //no scrolling = 1, half-way up the page = 0
+    $('.arrow-wrap').css('opacity', position);
+});
+
+
+//Code (((stolen))) from css-tricks for smooth scrolling:
+$(function () {
+    $('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
