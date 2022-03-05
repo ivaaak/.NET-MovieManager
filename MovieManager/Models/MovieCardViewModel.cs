ddb0b7dca5b59ee96a92e14f041386b57f047566
ivaaak <201218024@fdiba.tu-sofia.bg>
2022-03-05 23:40:33 +0200

    actor view fixed + added roles list

--- MovieManager/Models/MovieCardViewModel.cs
+++ MovieManager/Models/MovieCardViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using MovieManager.Data.DataModels;
+using TMDbLib.Objects.People;
 
 namespace MovieManager.Models
 {
     public class MovieCardViewModel
     {
         public Movie Movie { get; set; }
+
+        public MovieCredits MovieCredits { get; set; }
     }
 }
