ce60c6f3eccaae7c3b4b5b11d2887266189cf838
ivaaak <201218024@fdiba.tu-sofia.bg>
2022-03-24 16:40:37 +0200

    cleaned up program.cs -> added serviceCollection where all services and contexts are reg'd

---
+++ MovieManager/Infrastructure/ServiceCollectionExtension.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MovieManager.Data;
+using MovieManager.Data.DataModels;
+using MovieManager.Data.DBConfig;
+using MovieManager.Services;
+using MovieManager.Services.MovieServices;
+using MovieManager.Services.Repositories;
+using MovieManager.Services.ServicesContracts;
+
+namespace MovieManager.Infrastructure
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            //repo
+            services.AddScoped<IApplicationDbRepository, ApplicationDbRepository>();
+            //services
+            services.AddScoped<ISearchMethodsService, SearchMethodsService>();
+            services.AddScoped<IAddToDbService, AddToDbService>();
+            services.AddScoped<IGetFromDbService, GetFromDbService>();
+            services.AddScoped<ISaveMovieToDbObjectService, SaveMovieToDbObjectService>();
+            services.AddScoped<IDeleteFromDbService, DeleteFromDbService>();
+            services.AddScoped<IApiGetListsService, ApiGetListsService>();
+
+            services.AddControllersWithViews();
+
+            return services;
+        }
+
+
+        public static IServiceCollection AddApplicationDbContexts(this IServiceCollection services)
+        {
+            //Add connection string
+            services.AddDbContext<MovieContext>(options => 
+                options.UseSqlServer(Configuration.ConnectionString));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+
+
+        public static IServiceCollection AddIdentityContext(this IServiceCollection services)
+        {
+            //Add DB and Identity Services
+            services.AddDefaultIdentity<User>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.SignIn.RequireConfirmedEmail = false;
+                options.SignIn.RequireConfirmedAccount = false;
+            })
+            .AddRoles<IdentityRole>().AddEntityFrameworkStores<MovieContext>();
+
+            return services;
+        }
+    }
+}
--- MovieManager/Program.cs
+++ MovieManager/Program.cs
@@ -1,46 +1,21 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using MovieManager.Data;
-using MovieManager.Data.DataModels;
-using MovieManager.Data.DBConfig;
-using MovieManager.Services;
-using MovieManager.Services.MovieServices;
-using MovieManager.Services.Repositories;
-using MovieManager.Services.ServicesContracts;
-
+using MovieManager.Infrastructure;
 
 //Builder
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-//Add connection string
-builder.Services.AddDbContext<MovieContext>(options => options.UseSqlServer(Configuration.ConnectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+//Add DbContext
+builder.Services.AddApplicationDbContexts();
 
 //Add DB and Identity Services
-builder.Services.AddDefaultIdentity<User>(options =>
-{
-    options.Password.RequireDigit = false;
-    options.Password.RequireLowercase = false;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequireUppercase = false;
-    options.SignIn.RequireConfirmedEmail = false;
-    options.SignIn.RequireConfirmedAccount = false;
-})
-.AddRoles<IdentityRole>().AddEntityFrameworkStores<MovieContext>();
+builder.Services.AddIdentityContext();
 
 //Custom Services
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddScoped<IApplicationDbRepository, ApplicationDbRepository>();
-builder.Services.AddScoped<ISearchMethodsService, SearchMethodsService>();
-builder.Services.AddScoped<IAddToDbService, AddToDbService>();
-builder.Services.AddScoped<IGetFromDbService, GetFromDbService>();
-builder.Services.AddScoped<ISaveMovieToDbObjectService, SaveMovieToDbObjectService>();
-builder.Services.AddScoped<IDeleteFromDbService, DeleteFromDbService>();
-builder.Services.AddScoped<IApiGetListsService, ApiGetListsService>();
+builder.Services.AddApplicationServices();
 
 //Build
 WebApplication app = builder.Build();
+
 
 //Http request pipeline
 if (app.Environment.IsDevelopment()) {
