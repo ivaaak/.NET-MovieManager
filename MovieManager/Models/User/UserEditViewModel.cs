fb8b326339b5025655a6587161b27c9249ec762d
ivaaak <201218024@fdiba.tu-sofia.bg>
2022-03-27 12:02:20 +0300

    added user-related viewmodels, cleaned up unused ones

---
+++ MovieManager/Models/User/UserEditViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieManager.Models.User
+{
+    public class UserEditViewModel
+    {
+        public string Id { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string? LastName { get; set; }
+    }
+}
