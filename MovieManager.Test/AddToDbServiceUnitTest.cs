a0936c30c9d285aacb2f049d80ce8bfbbab95e02
ivaaak <ivaylo.pavlov@fdiba.tu-sofia.bg>
2022-03-30 18:59:03 +0300

    added tests project for unit tests, with empty classes for all service test methods

---
+++ MovieManager.Test/AddToDbServiceUnitTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace MovieManager.Test
+{
+    public class AddToDbServiceUnitTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
