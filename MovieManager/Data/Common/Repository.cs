a61542fbbd7c6044422d6d292bf449b07a911ff8
ivaaak <ivaylo.pavlov@fdiba.tu-sofia.bg>
2022-04-04 17:49:28 +0300

    cleaned up repo

--- MovieManager/Data/Common/Repository.cs
+++ MovieManager/Data/Common/Repository.cs
@@ -5,50 +5,26 @@ using System.Linq.Expressions;
 
 namespace MovieManager.Data.Common
 {
-    /// <summary>
-    /// Implementation of repository access methods
-    /// for Relational Database Engine
-    /// </summary>
-    /// <typeparam name="T">Type of the data table to which 
-    /// current reposity is attached</typeparam>
     public abstract class Repository : IRepository
     {
-        /// <summary>
-        /// Entity framework DB context holding connection information and properties
-        /// and tracking entity states 
-        /// </summary>
         protected DbContext Context { get; set; }
 
-        /// <summary>
-        /// Representation of table in database
-        /// </summary>
         protected DbSet<T> DbSet<T>() where T : class
         {
             return this.Context.Set<T>();
         }
 
-        /// <summary>
-        /// Adds entity to the database
-        /// </summary>
-        /// <param name="entity">Entity to add</param>
         public async Task AddAsync<T>(T entity) where T : class
         {
             await DbSet<T>().AddAsync(entity);
         }
 
-        /// <summary>
-        /// Ads collection of entities to the database
-        /// </summary>
-        /// <param name="entities">Enumerable list of entities</param>
         public async Task AddRangeAsync<T>(IEnumerable<T> entities) where T : class
         {
             await DbSet<T>().AddRangeAsync(entities);
         }
 
-        /// <summary>
-        /// All records in a table
-        /// </summary>
-        /// <returns>Queryable expression tree</returns>
+
         public IQueryable<T> All<T>() where T : class
         {
             return DbSet<T>().AsQueryable();
@@ -59,10 +35,7 @@ namespace MovieManager.Data.Common
             return this.DbSet<T>().Where(search).AsQueryable();
         }
 
-        /// <summary>
-        /// The result collection won't be tracked by the context
-        /// </summary>
-        /// <returns>Expression tree</returns>
+
         public IQueryable<T> AllReadonly<T>() where T : class
         {
             return this.DbSet<T>()
@@ -77,10 +50,7 @@ namespace MovieManager.Data.Common
                 .AsNoTracking();
         }
 
-        /// <summary>
-        /// Deletes a record from database
-        /// </summary>
-        /// <param name="id">Identificator of record to be deleted</param>
+
         public async Task DeleteAsync<T>(object id) where T : class
         {
             T entity = await GetByIdAsync<T>(id);
@@ -88,10 +58,7 @@ namespace MovieManager.Data.Common
             Delete<T>(entity);
         }
 
-        /// <summary>
-        /// Deletes a record from database
-        /// </summary>
-        /// <param name="entity">Entity representing record to be deleted</param>
+
         public void Delete<T>(T entity) where T : class
         {
             EntityEntry entry = this.Context.Entry(entity);
@@ -104,10 +71,7 @@ namespace MovieManager.Data.Common
             entry.State = EntityState.Deleted;
         }
 
-        /// <summary>
-        /// Detaches given entity from the context
-        /// </summary>
-        /// <param name="entity">Entity to be detached</param>
+
         public void Detach<T>(T entity) where T : class
         {
             EntityEntry entry = this.Context.Entry(entity);
@@ -115,21 +79,13 @@ namespace MovieManager.Data.Common
             entry.State = EntityState.Detached;
         }
 
-        /// <summary>
-        /// Disposing the context when it is not neede
-        /// Don't have to call this method explicitely
-        /// Leave it to the IoC container
-        /// </summary>
+
         public void Dispose()
         {
             this.Context.Dispose();
         }
 
-        /// <summary>
-        /// Gets specific record from database by primary key
-        /// </summary>
-        /// <param name="id">record identificator</param>
-        /// <returns>Single record</returns>
+
         public async Task<T> GetByIdAsync<T>(object id) where T : class
         {
             return await DbSet<T>().FindAsync(id);
@@ -140,37 +96,25 @@ namespace MovieManager.Data.Common
             return await DbSet<T>().FindAsync(id);
         }
 
-        /// <summary>
-        /// Saves all made changes in trasaction
-        /// </summary>
-        /// <returns>Error code</returns>
+
         public int SaveChanges()
         {
             return this.Context.SaveChanges();
         }
 
-        /// <summary>
-        /// Saves all made changes in trasaction
-        /// </summary>
-        /// <returns>Error code</returns>
+
         public async Task<int> SaveChangesAsync()
         {
             return await this.Context.SaveChangesAsync();
         }
 
-        /// <summary>
-        /// Updates a record in database
-        /// </summary>
-        /// <param name="entity">Entity for record to be updated</param>
+
         public void Update<T>(T entity) where T : class
         {
             this.DbSet<T>().Update(entity);
         }
 
-        /// <summary>
-        /// Updates set of records in the database
-        /// </summary>
-        /// <param name="entities">Enumerable collection of entities to be updated</param>
+
         public void UpdateRange<T>(IEnumerable<T> entities) where T : class
         {
             this.DbSet<T>().UpdateRange(entities);
