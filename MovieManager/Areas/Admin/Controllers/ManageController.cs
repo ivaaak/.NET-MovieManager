dc7cccf2d3c012de8807c09ad0e577140c9b4136
ivaaak <ivaylo.pavlov@fdiba.tu-sofia.bg>
2022-04-08 22:45:25 +0300

    added admin manage pages for playlists and movies

---
+++ MovieManager/Areas/Admin/Controllers/ManageController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieManager.Data;
+using MovieManager.Models;
+
+namespace MovieManager.Areas.Admin.Controllers
+{
+    public class ManageController : BaseController
+    {
+        private readonly MovieContext dataContext;
+
+        public ManageController(MovieContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+        public IActionResult Movies()
+        {
+            var movies = dataContext.Movies.Select(x => x).ToList();
+
+            MovieListViewModel model = new MovieListViewModel()
+            {
+                MoviesList = movies,
+            };
+            return View(model);
+        }
+        public IActionResult Playlists()
+        {
+            var playlists = dataContext.Playlists.Include(a => a.User).Select(x => x).ToList();
+
+            PlaylistsViewModel model = new PlaylistsViewModel()
+            {
+                Playlists = playlists,
+            };
+            return View(model);
+        }
+    }
+}
