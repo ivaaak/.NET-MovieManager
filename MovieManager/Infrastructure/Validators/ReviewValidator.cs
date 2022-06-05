26412109ee0c54ea84ae21c80ca29ed701c78692
ivaaak <ivaylo.pavlov@fdiba.tu-sofia.bg>
2022-06-06 00:11:42 +0300

    Added fluent validation to the Review Viewmodel.

---
+++ MovieManager/Infrastructure/Validators/ReviewValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using MovieManager.Models;
+
+namespace MovieManager.Infrastructure.Validators
+{
+    public class ReviewValidator : AbstractValidator<ReviewViewModel>
+    {
+        public ReviewValidator()
+        {
+            RuleFor(x => x.ReviewId).NotNull().NotEmpty();
+
+            RuleFor(x => x.ReviewTitle).NotNull().NotEmpty().WithMessage("Review title cannot be null");
+
+            RuleFor(x => x.ReviewContent).NotNull().NotEmpty().WithMessage("Review Content cannot be null");
+            RuleFor(x => x.ReviewContent).Length(10, 3000).WithMessage("Review Content has to be between 10 and 3000 characters long.");
+
+
+            RuleFor(x => x.MovieTitle).NotNull().NotEmpty().WithMessage("Movie title cannot be null");
+
+            RuleFor(x => x.Rating).NotNull().NotEmpty().WithMessage("Rating is required!");
+            RuleFor(x => x.Rating).InclusiveBetween(1, 10).WithMessage("Rating has to be between 1 and 10");
+            RuleFor(x => x.Rating).ScalePrecision(2, 4).WithMessage("Rating has to be a decimal number!"); 
+            //Checks if it is the same type as the property
+        }
+    }
+}
---
+++ MovieManager/Models/ReviewViewModel.cs
@@ -0,0 +1,17 @@
+namespace MovieManager.Models
+{
+    public class ReviewViewModel
+    {
+        public string ReviewId { get; set; }
+
+        public string ReviewTitle { get; set; }
+
+        public string ReviewContent { get; set; }
+
+        public decimal Rating { get; set; }
+
+        public int MovieId { get; set; }
+
+        public string MovieTitle { get; set; }
+    }
+}
