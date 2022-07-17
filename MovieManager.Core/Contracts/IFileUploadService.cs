afb4d2ff0f1de5f75f0bf9c594a5f478ddd498a5
ivaaak <ivaylo.pavlov@fdiba.tu-sofia.bg>
2022-07-17 09:55:09 +0300

    Moved FileUploadService to the Core project.

---
+++ MovieManager.Core/Contracts/IFileUploadService.cs
@@ -0,0 +1,9 @@
+﻿using MovieManager.Infrastructure.Data.Models;
+
+namespace MovieManager.Core.Contracts
+{
+    public interface IFileUploadService
+    {
+        Task SaveFileAsync(ApplicationFile file);
+    }
+}
---
+++ MovieManager.Core/Services/FileUploadService.cs
@@ -0,0 +1,22 @@
+﻿using MovieManager.Core.Contracts;
+using MovieManager.Infrastructure.Data.Models;
+using MovieManager.Infrastructure.Repositories;
+
+namespace MovieManager.Services
+{
+    public class FileUploadService : IFileUploadService
+    {
+        private readonly IApplicationDbRepository repo;
+
+        public FileUploadService(IApplicationDbRepository _repo)
+        {
+            repo = _repo;
+        }
+
+        public async Task SaveFileAsync(ApplicationFile file)
+        {
+            await repo.AddAsync(file);
+            await repo.SaveChangesAsync();
+        }
+    }
+}
